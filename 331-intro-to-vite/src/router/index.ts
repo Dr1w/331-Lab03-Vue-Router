abd719601f828efce6e209a87a5c3640e770173d
Drew <bqian776@gmail.com>
2024-07-25 21:11:59 +0700

    #1.7 Click on events then jump to new page

--- 331-intro-to-vite/src/router/index.ts
+++ 331-intro-to-vite/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import EventListView from '@/views/EventListView.vue'
 import AboutView from '@/views/AboutView.vue'
 import StudentView from '@/views/StudentListView.vue'
+import EventDetailView from '@/views/EventDetailView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -23,9 +24,10 @@ const router = createRouter({
 
     },
     {
-      path: '/event/5928101',
+      path: '/event/:id',
       name: 'event-detail-view',
-      component: EventListView
+      component: EventDetailView,
+      props: true
     },
   ]
 })
