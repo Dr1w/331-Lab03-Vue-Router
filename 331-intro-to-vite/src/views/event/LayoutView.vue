c526839e48b7c8e1ff4dc31c206cd72fbf27ca5b
Drew <bqian776@gmail.com>
2024-07-29 17:47:39 +0700

    Check Point #5.2 Close internet connection shown NetworkError view

--- 331-intro-to-vite/src/views/event/LayoutView.vue
+++ 331-intro-to-vite/src/views/event/LayoutView.vue
@@ -3,6 +3,7 @@ import { ref, onMounted } from 'vue'
 import { type Event } from '@/types'
 import EventService from '@/services/EventService'
 import { useRouter } from 'vue-router';
+import { error } from 'console';
 
 const event = ref<Event | null>(null)
 const props = defineProps({
@@ -18,11 +19,15 @@ onMounted(() => {
     .then((response) => {
       event.value = response.data
     })
-    .catch(() => {
-      router.push({
-        name: '404-resource-view',
-        params: { resource: 'event' }
-      })
+    .catch((error) => {
+        if (error.response && error.response.status === 404){
+            router.push({
+                name: '404-resource-view',
+                params: { resource : 'event'}
+            })
+        } else {
+            router.push({ name: 'network-error-view'})
+        }
     })
 })
 </script>
